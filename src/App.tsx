d285ff34b143c236916f31d0fafe749bf317a321
Carlos Ramirez <carlos@extrasistemas.com>
2023-04-05 08:59:11 -0600

    feat: add props in the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { RandomFox } from "./RandomFox";
 import "./App.css";
+const random = () => Math.floor(Math.random() * 100);
+const image = `https://randomfox.ca/images/${random()}.jpg`;
 
 function App() {
   return (
     <>
       <h1 className="text-xl font-bold">Random Fox</h1>
-      <RandomFox />;
+      <RandomFox image={image} />
     </>
   );
 }
-
 export default App;
--- src/RandomFox.tsx
+++ src/RandomFox.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-const random = () => Math.floor(Math.random() * 100);
 
-export const RandomFox = (): JSX.Element => {
-  const img = `https://randomfox.ca/images/${random()}.jpg`;
-  return <img width={320} height="auto" className="rounded" src={img} />;
+type Props = { image: string };
+export const RandomFox = ({ image }: Props): JSX.Element => {
+  return <img width={320} height="auto" className="rounded" src={image} />;
 };
