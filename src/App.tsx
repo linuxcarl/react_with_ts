e668e5217a1ca08d0a3427d194eb512a1e254b37
Carlos Ramirez <carlos@extrasistemas.com>
2023-04-09 17:31:45 -0600

    refactor: add types in array of objects

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,24 @@ import { RandomFox } from "./RandomFox";
 import "./App.css";
 const random = () => Math.floor(Math.random() * 100);
 
+const generateId = () => {
+  return Math.random().toString(36).substr(2, 9);
+};
+type imageItem = { id: string; url: string };
 function App() {
   // const image = `https://randomfox.ca/images/${random()}.jpg`;
-  const [images, setImage] = useState([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
+  const [images] = useState<Array<imageItem>>([
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
   ]);
   return (
     <>
       <h1 className="text-xl font-bold">Random Fox</h1>
-      {images.map((image, index) => (
-        <div key={index} className="p-4">
-          <RandomFox image={image} />
+      {images.map((image) => (
+        <div key={image.id} className="p-4">
+          <RandomFox image={image.url} />
         </div>
       ))}
     </>
