24d38533b8bd1205d57f26074b60a0a16cb282fb
Carlos Ramirez <carlos@extrasistemas.com>
2023-04-09 17:12:13 -0600

    feat: add for images with lopp

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { RandomFox } from "./RandomFox";
 import "./App.css";
 const random = () => Math.floor(Math.random() * 100);
-const image = `https://randomfox.ca/images/${random()}.jpg`;
 
 function App() {
+  // const image = `https://randomfox.ca/images/${random()}.jpg`;
+  const [images, setImage] = useState([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
   return (
     <>
       <h1 className="text-xl font-bold">Random Fox</h1>
-      <RandomFox image={image} />
+      {images.map((image, index) => (
+        <div key={index} className="p-4">
+          <RandomFox image={image} />
+        </div>
+      ))}
     </>
   );
 }
